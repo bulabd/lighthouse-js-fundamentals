aec5a47d64ebf72cbc6c24da40bcbc8cd060f22e
bulabd <bulabd@outlook.com>
2022-03-14 19:32:38 +0000

    ice-cream challenge solution

---
+++ iceCream.js
@@ -0,0 +1,10 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+iceCreamFlavours.push('root beer'); // add flavour
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]); // get the first flavour
+
+console.log(iceCreamFlavours[5]); // get the last flavour
+
+console.log(iceCreamFlavours.length); // total number of flavours
