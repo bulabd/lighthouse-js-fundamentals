3bbef5a4e38b9f20f7e9b94c3a7d875503b4c157
bulabd <bulabd@outlook.com>
2022-03-15 16:18:45 +0000

    Updated solution

--- lastIndexOf.js
+++ lastIndexOf.js
@@ -1,5 +1,10 @@
 function lastIndexOf(array, element) {
-  return array.lastIndexOf(element);
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (array[i] === element) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
@@ -7,3 +12,7 @@ console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
 console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
 console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
 console.log(lastIndexOf([], 3), "=?", -1);
+
+/*
+return array.lastIndexOf(element);
+*/
